4d72ab5b8bea81f2ca01d785803f548b20bcfbef
MarijusIL <78425010+MarijusIL@users.noreply.github.com>
2021-09-30 06:39:42 +0300

    added a route with a wildcard

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,10 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 
 class DefaultController {
-
+    /**
+     * @Route("/")
+     */
     public function index() {
         return new Response('Hello World');
+    }
+    /**
+     * @Route("/user/{username}")
+     */
+    public function show($username) {
+        return new Response("Showing data of $username");
     }
 }
